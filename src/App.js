89b88a99e96f46574d11cb4e75447dadfd3308d2
jack.gundrum <jpgundrum@wisc.edu>
2022-05-21 14:53:55 -0500

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import {Helmet} from "react-helmet";
+//import logo from './logo.svg';
 import './App.css';
+import './myStyles.css'
+import image from './badger_pic.png'
+
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
+  const [greeting, setGreetingValue] = useState('')
+
+   // request access to the user's MetaMask account
+  async function requestAccount(){
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+
+   // call the smart contract, read the current greeting value
+  async function fetchGreeting(){
+    if (typeof window.ethereum !== 'undefined') {                               // line that checks if the user has Metamask installed
+      const provider = new ethers.providers.Web3Provider(window.ethereum)       // step in obtaining contract var which can call the Greeter.sol methods
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()     // obtain current set Greeting
+        console.log('data: ', data)             // print out set Greeting
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    } 
+  }
+
+// call the smart contract, send an update
+  async function setGreeting(){
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+
+      // We are updating the blockchain, therefore we needed to add another step when creating the contract.
+      // We need to have a way to create a transaction. In order to do that we need to sign the transaction using a signer
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()                                     // signs
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer) // notice how it is "signer" for a change on the blockchain
+      const transaction = await contract.setGreeting(greeting)
+      setGreetingValue('')
+      await transaction.wait()        // wait for the transaction to be confirmed on the blockchain; in a prod env this might take a while
+      fetchGreeting()                 // logs out greeting
+    }
+  }
+
+  
+  
   return (
     <div className="App">
+    <Helmet>
+      <title>Badger Blocks</title>
+    </Helmet>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={image} alt=""/>
+      <h1 className ='primary'>Greetings using Blockchain</h1>
+        <button className="btn_props" onClick={fetchGreeting}>Fetch Greeting</button> {/*when button is clicked it invokes the fetch Greeting method */}
+        <button className="btn_props" onClick={setGreeting}>Set Greeting</button>     {/*when button is clicked it invokes the set Greeting method */}
+        <input className="text_box"
+          onChange={e => setGreetingValue(e.target.value)}
+          placeholder="Set greeting"
+          value={greeting}
+        />
       </header>
     </div>
   );
